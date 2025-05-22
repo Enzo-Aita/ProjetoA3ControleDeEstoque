32236175e6c092bdb2aa162780d40fa7dd321288
Enzo-Aita <Enzoaitanaime99@gmail.com>
2025-05-22 09:48:03 -0300

    ControleEstoque

--- src/main/java/visao/FrmMenuPrincipal.java
+++ src/main/java/visao/FrmMenuPrincipal.java
@@ -1,18 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package visao;
 
-/**
- *
- * @author andre
- */
+
 public class FrmMenuPrincipal extends javax.swing.JFrame {
 
-    /**
-     * Creates new form FrmMenuPrincipal
-     */
+    
     public FrmMenuPrincipal() {
         initComponents();
     }
